082f0f3699b4018a01d6ac7c13950628455b2ed1
Amod Vardhan <amod.vardhan002@gmail.com>
2023-01-11 12:41:59 +0530

    loading individual projects with faker and shared webpack

--- ecomm/cart/src/index.js
+++ ecomm/cart/src/index.js
@@ -1,7 +1,1 @@
-import faker from "faker";
-
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
-
-document.querySelector("#dev-cart").innerHTML = cartText;
-
-console.log(cartText);
+import("./bootstrap");
